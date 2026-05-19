1f9d2cc6342b2042b6515a05cced289c4484af74
pranav244872 <aekbotepranav25@gmail.com>
2026-05-20 01:19:53 +0530

    fix: fix broken User.java setter syntax

--- src/main/java/com/pranav244872/fitness_tracker/model/User.java
+++ src/main/java/com/pranav244872/fitness_tracker/model/User.java
@@ -77,10 +77,9 @@ public class User implements UserDetails {
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
     public void setUsername(String username) { this.username = username; }
-        this.email = email;
-    public void setPassword(String password) { this.password = password; }
-    public Role getRole() { return role; }
     public String getEmail() { return email; }
     public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public Role getRole() { return role; }
     public void setRole(Role role) { this.role = role; }
 }
